42ff01dcc0ee99b708562d3968a319317eac46f4
Starforcraft <starforcraft@yahoo.com>
2026-08-22 10:19:26 +0200

    Fixed publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import me.modmuss50.mpp.PublishModTask
 plugins {
     id("com.refinedmods.refinedarchitect.root")
     id("com.refinedmods.refinedarchitect.base")
-    id("me.modmuss50.mod-publish-plugin") version "1.1.0"
+    id("me.modmuss50.mod-publish-plugin") version "2.1.1"
 }
 
 refinedarchitect {
@@ -36,6 +36,8 @@ publishMods {
         accessToken = providers.environmentVariable("CURSEFORGE_TOKEN")
         projectId = "454382"
         minecraftVersions.add(minecraftVersion)
+        client = true
+        server = true
     }
 
     val mrOptions = modrinthOptions {
